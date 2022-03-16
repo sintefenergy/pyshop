3c21b0d64adab6a42b942c25a5d902f2eb82f455
Per Aaslid <48120302+peraaslid@users.noreply.github.com>
2022-03-16 17:28:11 +0100

    Test get timeres

--- tests/test_shop_api.py
+++ tests/test_shop_api.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from pyshop.shopcore.shop_api import get_attribute_value, set_attribute
+from pyshop.shopcore.shop_api import get_attribute_value, get_time_resolution, set_attribute
 
 
 class ShopApiMock:
@@ -169,3 +169,13 @@ class TestSetAttribute:
         assert res[3].startswith(self.shop_api['GetStartTime'])
         assert (res[4] == self.shop_api['GetTxySeriesT'][0:1]).all()
         assert (res[5] == [1.1]).all()
+
+
+class TestTime:
+    shop_api = ShopApiMock()
+
+    def test_get_time_resolution(self):
+        timeres = get_time_resolution(self.shop_api)
+        assert timeres['starttime'] == pd.Timestamp(self.shop_api['GetStartTime'])
+        assert timeres['endtime'] == pd.Timestamp(self.shop_api['GetEndTime'])
+        assert timeres['timeunit'] == self.shop_api['GetTimeUnit']
